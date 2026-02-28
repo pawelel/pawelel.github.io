bded534a6b29b27465ad3e9f27bd27398e1db94d
Paweł Przytuła <pawel.przytula@m2t.com.pl>
2026-02-28 11:14:02 +0100

    Update tech stack icons to use @astro-icons and local SVGs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://pawelel.github.io',
   output: 'static',
+  integrations: [icon()],
 });
